e0869e79b7f87d07bdadf45ca68cfadcf7f9999c
Sam Portelli <sampor@bigpond.com>
2021-08-15 14:03:03 +1000

    Initial Commit

---
+++ testCode.py
@@ -0,0 +1,15 @@
+mon_sales = "121"
+tues_sales = "105"
+wed_sales = "110"
+thurs_sales = "98"
+fri_sales = "95"
+print (int(mon_sales) + int(tues_sales))
+#total_sales = int(mon_sales)
+#total_sales = str(mon_sales) + str(tues_sales) + str(wed_sales) + str(thurs_sales) + str(fri_sales)
+#print("This week's total sales: " + total_sales)
+#TODO: Print a string with this format: This week's total sales: xxx
+# You will probably need to write some lines of code before the print statement.
+
+def multiply(a, b):
+  return a * b 
+print(multiply(3,4))
